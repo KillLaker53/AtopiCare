23caadef2a50da0b03ad8d729a1695e1b673a55b
PepiDianabad <petar.y.petrov@elsys-bg.org>
2025-03-20 21:54:09 +0200

    Merge branch 'main' of https://github.com/KillLaker53/Hack-Tuah-11

--- backend/src/schema/reply.schema.ts
+++ backend/src/schema/reply.schema.ts
@@ -1,18 +1,15 @@
 import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
 
-@Schema({ _id: false })
+@Schema()
 export class Reply {
   @Prop({ type: Number })
   user_id: number;
 
   @Prop({ type: String })
-  message: string;
+  content: string;
 
   @Prop({ type: String })
   date: string;
-
-  @Prop({ type: [Reply] })
-  replies: Reply[];
 }
 
 export const ReplySchema = SchemaFactory.createForClass(Reply);
