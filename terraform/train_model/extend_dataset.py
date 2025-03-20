91ec350fda3df7689d65613819f9f121e2393835
PepiDianabad <petar.y.petrov@elsys-bg.org>
2025-03-20 19:06:17 +0200

    traning model - RESNET50

---
+++ terraform/train_model/extend_dataset.py
@@ -0,0 +1,50 @@
+import os
+import cv2
+import numpy as np
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+dataset_dir = "atopic-dataset"
+
+datagen = ImageDataGenerator(
+    rotation_range=40, 
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,  
+    zoom_range=0.2, 
+    horizontal_flip=True,
+    brightness_range=[0.7, 1.3], 
+    fill_mode="nearest"  
+)
+
+
+for category in sorted(os.listdir(dataset_dir)):
+    category_path = os.path.join(dataset_dir, category)
+    if not os.path.isdir(category_path):
+        continue  
+
+    print(f"📂 Обработваме категория: {category}")
+
+
+    for filename in os.listdir(category_path):
+        file_path = os.path.join(category_path, filename)
+        if not filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+            continue  
+
+   
+        image = cv2.imread(file_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 
+        image = np.expand_dims(image, axis=0)  
+
+    
+        i = 0
+        for batch in datagen.flow(image, batch_size=1):
+            new_filename = f"{filename.split('.')[0]}_aug{i}.jpg"
+            new_filepath = os.path.join(category_path, new_filename)
+            cv2.imwrite(new_filepath, cv2.cvtColor(batch[0], cv2.COLOR_RGB2BGR)) 
+            i += 1
+            if i >= 5:  
+                break
+
+    print(f"✅ Завършено за {category}!\n")
+
+print("🎉 Dataset-ът е разширен успешно! 🚀")
---
+++ terraform/train_model/train.py
@@ -0,0 +1,101 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import ResNet50V2
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+#Early Stopping and Checkpoint
+early_stopping = EarlyStopping(monitor="val_loss", patience=5, restore_best_weights=True)
+checkpoint = ModelCheckpoint("best_resnet_model.h5", monitor="val_accuracy", save_best_only=True)
+
+train_dir = "atopic-dataset"
+test_dir = "atopic-dataset"
+
+# image gen with augmention
+train_datagen = ImageDataGenerator(
+    rescale=1.0 / 255,
+    rotation_range=30,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    brightness_range=[0.7, 1.3],
+    validation_split=0.2
+)
+
+test_datagen = ImageDataGenerator(rescale=1.0 / 255)
+
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode="categorical",
+    subset="training"
+)
+
+val_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode="categorical",
+    subset="validation"
+)
+
+test_generator = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode="categorical",
+    shuffle=False
+)
+
+base_model = ResNet50V2(weights="imagenet", include_top=False)
+
+#personalized layers
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(256, activation="relu")(x) 
+x = Dropout(0.4)(x) 
+predictions = Dense(5, activation="softmax")(x)
+
+
+model = Model(inputs=base_model.input, outputs=predictions)
+
+for layer in base_model.layers:
+    layer.trainable = False
+
+model.compile(
+    optimizer=Adam(learning_rate=0.001),
+    loss="categorical_crossentropy",
+    metrics=["accuracy"]
+)
+
+history = model.fit(
+    train_generator,
+    epochs=20,
+    validation_data=val_generator,
+    callbacks=[early_stopping, checkpoint]
+)
+
+for layer in base_model.layers[-30:]:
+    layer.trainable = True
+
+model.compile(
+    optimizer=Adam(learning_rate=0.00005),
+    loss="categorical_crossentropy",
+    metrics=["accuracy"]
+)
+
+history_finetune = model.fit(
+    train_generator,
+    epochs=10,
+    validation_data=val_generator,
+    callbacks=[early_stopping, checkpoint]
+)
+
+test_loss, test_acc = model.evaluate(test_generator)
+print(f"🎯 Test Accuracy: {test_acc * 100:.2f}%")
