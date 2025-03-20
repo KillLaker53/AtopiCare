01999b45d8ee31e113a8f2aef40c33e072aa180f
KillLaker53 <ssis.kn@gmail.com>
2025-03-21 00:34:49 +0200

    fix: resolved merge conflicts

--- AtopiCare/app/(tabs)/_layout.tsx
+++ AtopiCare/app/(tabs)/_layout.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Platform, View, useColorScheme } from 'react-native';
 import AuthScreen from './AuthScreen';
@@ -14,90 +13,5 @@ export default function TabLayout() {
   //           <AuthScreen />
   //       </View>
   // );
-=======
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
->>>>>>> dcc418dbe7d0e116c8df53d4177c1786160860cc
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-<<<<<<< HEAD
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="forum"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-      {/* <Tabs.Screen
-        name="profile"
-        options={{
-          title: 'Profile',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
-        }}
-      /> */}
-      <Tabs.Screen
-        name="UploadPhoto"
-        options={{
-          title: 'Upload Photo',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-=======
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
->>>>>>> dcc418dbe7d0e116c8df53d4177c1786160860cc
-  );
+ 
 }
--- AtopiCare/app/(tabs)/forum.tsx
+++ AtopiCare/app/(tabs)/forum.tsx
@@ -1,401 +1,212 @@
-import React, { useState } from 'react';
-<<<<<<< HEAD
-import { View, Text, TextInput, Button, StyleSheet, FlatList, TouchableOpacity, Modal, ScrollView, Image } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-=======
-import { View, Text, TextInput, Button, StyleSheet, FlatList, TouchableOpacity, Modal, ScrollView } from 'react-native';
->>>>>>> dcc418dbe7d0e116c8df53d4177c1786160860cc
+import React, { useState } from "react";
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    StyleSheet,
+    Alert,
+    Dimensions
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { LinearGradient } from "expo-linear-gradient";
 
-type Thread = {
-  id: number;
-  title: string;
-  content: string;
-  user: string;
-  date: string;
-  replies: { content: string; user: string; date: string }[];
+type RootStackParamList = {
+    AuthScreen: undefined;
+    UploadPhotoScreen: undefined;
 };
 
-const Forum: React.FC = () => {
-  const [threads, setThreads] = useState<Thread[]>([]);
-  const [newThreadTitle, setNewThreadTitle] = useState('');
-  const [newThreadContent, setNewThreadContent] = useState('');
-  const [selectedThread, setSelectedThread] = useState<Thread | null>(null);
-  const [replyContent, setReplyContent] = useState('');
-<<<<<<< HEAD
-  const [images, setImages] = useState<string[]>([]);
-=======
->>>>>>> dcc418dbe7d0e116c8df53d4177c1786160860cc
+type AuthScreenNavigationProp = StackNavigationProp<RootStackParamList, "UploadPhotoScreen">;
 
-  const handlePostThread = () => {
-    const newThread: Thread = {
-      id: threads.length + 1,
-      title: newThreadTitle,
-      content: newThreadContent,
-      user: 'User1',
-      date: new Date().toLocaleString(),
-      replies: [],
-    };
-    setThreads([...threads, newThread]);
-    setNewThreadTitle('');
-    setNewThreadContent('');
-<<<<<<< HEAD
-    setImages([]);
-  };
+const { width, height } = Dimensions.get("window");
 
-  const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsMultipleSelection: true,
-      quality: 1,
-    });
+export default function AuthScreen() {
+    const [isRegistering, setIsRegistering] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [username, setUsername] = useState("");
+    const [isDarkTheme, setIsDarkTheme] = useState(true);
 
-    if (!result.canceled) {
-      setImages([...images, result.assets[0].uri]);
-    }
-  };
+    const navigation = useNavigation<AuthScreenNavigationProp>();
 
-  return (
-      <View style={styles.container}>
-        <TextInput
-            style={styles.input}
-            placeholder="Thread Title"
-            value={newThreadTitle}
-            onChangeText={setNewThreadTitle}
-        />
-        <TextInput
-            style={[styles.input, styles.textArea]}
-            placeholder="Thread Content"
-            value={newThreadContent}
-            onChangeText={setNewThreadContent}
-            multiline={true}
-            numberOfLines={4}
-        />
-        <TouchableOpacity style={styles.imagePicker} onPress={pickImage}>
-          <Text style={styles.imagePickerText}>+</Text>
-        </TouchableOpacity>
-        <View style={styles.imageContainer}>
-          {images.map((image, index) => (
-              <Image key={index} source={{ uri: image }} style={styles.imagePreview} />
-          ))}
-        </View>
-        <Button title="Post Thread" onPress={handlePostThread} />
-        <FlatList
-            data={threads}
-            keyExtractor={item => item.id.toString()}
-            renderItem={({ item }) => (
-                <TouchableOpacity onPress={() => setSelectedThread(item)}>
-                  <View style={styles.threadItem}>
-                    <Text style={styles.threadTitle}>{item.title}</Text>
-                    <Text style={styles.threadInfo}>{item.user} - {item.date}</Text>
-                  </View>
-                </TouchableOpacity>
-            )}
-        />
-      </View>
-=======
-  };
-
-  const handlePostReply = () => {
-    const updatedThreads = threads.map(thread => {
-      if (thread.id === selectedThread?.id) {
-        return {
-          ...thread,
-          replies: [...thread.replies, { content: replyContent, user: 'User1', date: new Date().toLocaleString() }],
-        };
-      }
-      return thread;
-    });
-    setThreads(updatedThreads);
-    setReplyContent('');
-  };
-
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={threads}
-        keyExtractor={item => item.id.toString()}
-        ListHeaderComponent={
-          <View style={styles.newThreadContainer}>
-            <TextInput
-              style={styles.input}
-              placeholder="Thread Title"
-              value={newThreadTitle}
-              onChangeText={setNewThreadTitle}
-            />
-            <TextInput
-              style={[styles.input, styles.textArea]}
-              placeholder="Thread Content"
-              value={newThreadContent}
-              onChangeText={setNewThreadContent}
-              multiline={true}
-              numberOfLines={4}
-            />
-            <TouchableOpacity style={styles.button} onPress={handlePostThread}>
-              <Text style={styles.buttonText}>Post</Text>
-            </TouchableOpacity>
-          </View>
+    const handleRegister = () => {
+        if (!email || !password || !firstName || !lastName || !username) {
+            Alert.alert("Error", "Please fill in all fields.");
+            return;
         }
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => setSelectedThread(item)}>
-            <View style={styles.threadItem}>
-              <Text style={styles.threadTitle}>{item.title}</Text>
-              <Text style={styles.threadInfo}>{item.user} - {item.date}</Text>
-            </View>
-          </TouchableOpacity>
-        )}
-      />
+        Alert.alert("Success", "You have successfully registered!");
+        setIsRegistering(false);
+    };
 
-      {selectedThread && (
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={!!selectedThread}
-          onRequestClose={() => setSelectedThread(null)}
+    const handleLogin = () => {
+        if (!username || !password) {
+            Alert.alert("Error", "Please fill in both fields.");
+            return;
+        }
+        Alert.alert("Success", "You have successfully logged in!");
+        navigation.navigate("UploadPhotoScreen");
+    };
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            padding: width * 0.05,
+        },
+        appTitle: {
+            fontWeight: "bold",
+            fontSize: width * 0.08,
+            textAlign: "center",
+            color: isDarkTheme ? "#99ddff" : "#007AFF",
+            paddingVertical: height * 0.03,
+            fontFamily: "Garamond",
+        },
+        input: {
+            width: "85%",
+            padding: width * 0.04,
+            borderWidth: 1,
+            borderRadius: 30,
+            marginBottom: 12,
+            borderColor: isDarkTheme ? "#99ddff" : "#007AFF",
+            backgroundColor: isDarkTheme ? "rgba(153, 221, 255, 0.2)" : "rgba(0, 122, 255, 0.1)",
+            color: isDarkTheme ? "white" : "black",
+            fontSize: width * 0.04,
+        },
+        buttonPrimary: {
+            width: "60%",
+            paddingVertical: height * 0.02,
+            backgroundColor: "#007AFF",
+            borderRadius: 30,
+            marginTop: height * 0.02,
+            alignItems: "center",
+            shadowColor: "#007AFF",
+            shadowOpacity: 0.4,
+            shadowRadius: 5,
+            elevation: 5,
+        },
+        buttonSecondary: {
+            width: "60%",
+            paddingVertical: height * 0.02,
+            backgroundColor: isDarkTheme ? "rgba(255, 255, 255, 0.2)" : "rgba(0, 122, 255, 0.1)",
+            borderRadius: 30,
+            marginTop: height * 0.02,
+            alignItems: "center",
+            borderWidth: 1,
+            borderColor: isDarkTheme ? "#99ddff" : "#007AFF",
+        },
+        buttonText: {
+            color: "white",
+            fontSize: width * 0.05,
+            fontWeight: "bold",
+        },
+        buttonTextSecondary: {
+            color: isDarkTheme ? "#99ddff" : "#007AFF",
+            fontSize: width * 0.05,
+        },
+        icon: {
+            fontSize: width * 0.25,
+            marginBottom: height * 0.03,
+            marginTop: height * 0.01,
+            color: isDarkTheme ? "#99ddff" : "#007AFF",
+        },
+        iconTheme: {
+            fontSize: width * 0.07,
+            color: isDarkTheme ? "white" : "black",
+            position: "absolute",
+            bottom: height * 0.038,
+            left: width * 0.35,
+        },
+    });
+
+    return (
+        <LinearGradient
+            colors={isDarkTheme ? ["#0f0c29", "#302b63", "#24243e"] : ["#ffffff", "#f2f2f2"]}
+            style={styles.container}
         >
-          <View style={styles.modalView}>
-            <TouchableOpacity style={styles.closeButtonTopRight} onPress={() => setSelectedThread(null)}>
-              <Text style={styles.closeButtonText}>X</Text>
+            <Text style={styles.appTitle}>ATOPICARE</Text>
+
+            <TouchableOpacity onPress={() => setIsDarkTheme(!isDarkTheme)}>
+                <Ionicons
+                    style={styles.iconTheme}
+                    name={isDarkTheme ? "contrast" : "contrast-outline"}
+                />
             </TouchableOpacity>
-            <Text style={styles.modalTitle}>{selectedThread.title}</Text>
-            <Text style={styles.modalContent}>{selectedThread.content}</Text>
-            <Text style={styles.modalInfo}>{selectedThread.user} - {selectedThread.date}</Text>
-            <FlatList
-              data={selectedThread.replies}
-              keyExtractor={(item, index) => index.toString()}
-              renderItem={({ item }) => (
-                <View style={styles.replyItem}>
-                  <Text>{item.content}</Text>
-                  <Text style={styles.replyInfo}>{item.user} - {item.date}</Text>
-                </View>
-              )}
-            />
+
+            <Ionicons style={styles.icon} name="person-circle-outline" />
+
+            {isRegistering && (
+                <>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="First Name"
+                        placeholderTextColor={isDarkTheme ? "white" : "black"}
+                        value={firstName}
+                        onChangeText={setFirstName}
+                        keyboardType="default"
+                        autoCapitalize="words"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Last Name"
+                        placeholderTextColor={isDarkTheme ? "white" : "black"}
+                        value={lastName}
+                        onChangeText={setLastName}
+                        keyboardType="default"
+                        autoCapitalize="words"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Email"
+                        placeholderTextColor={isDarkTheme ? "white" : "black"}
+                        value={email}
+                        onChangeText={setEmail}
+                        keyboardType="email-address"
+                        autoCapitalize="none"
+                    />
+                </>
+            )}
+
             <TextInput
-              style={[styles.input, styles.textArea]}
-              placeholder="Write a reply..."
-              value={replyContent}
-              onChangeText={setReplyContent}
-              multiline={true}
-              numberOfLines={2}
+                style={styles.input}
+                placeholder="Username"
+                placeholderTextColor={isDarkTheme ? "white" : "black"}
+                value={username}
+                onChangeText={setUsername}
+                keyboardType="default"
+                autoCapitalize="none"
             />
-            <TouchableOpacity style={styles.button} onPress={handlePostReply}>
-              <Text style={styles.buttonText}>Post Reply</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={[styles.button, styles.closeButton]} onPress={() => setSelectedThread(null)}>
-              <Text style={styles.buttonText}>Close</Text>
-            </TouchableOpacity>
-          </View>
-        </Modal>
-      )}
-    </View>
->>>>>>> dcc418dbe7d0e116c8df53d4177c1786160860cc
-  );
-};
 
-const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    padding: 20,
-<<<<<<< HEAD
-    paddingTop: 40,
-=======
-    paddingTop: 40, 
->>>>>>> dcc418dbe7d0e116c8df53d4177c1786160860cc
-  },
-  newThreadContainer: {
-    marginBottom: 20,
-    backgroundColor: '#f9f9f9',
-    padding: 15,
-    borderRadius: 10,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    padding: 10,
-    marginBottom: 10,
-    borderRadius: 5,
-  },
-<<<<<<< HEAD
-  textAreaContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  imageContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    marginBottom: 10,
-  },
-  textArea: {
-    flex: 1,
-    textAlignVertical: 'top',
-  },
-  imagePicker: {
-    marginLeft: 10,
-    backgroundColor: '#ddd',
-    padding: 10,
-    borderRadius: 5,
-  },
-  imagePickerText: {
-    fontSize: 20,
-    color: '#555',
-  },
-  imagePreview: {
-    width: 100,
-    height: 100,
-    marginTop: 10,
-    borderRadius: 5,
-  },
-  button: {
-    backgroundColor: '#007BFF',
-    padding: 10,
-    borderRadius: 5,
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
-  },
-=======
-  textArea: {
-    height: 100, 
-    textAlignVertical: 'top', 
-  },
-  
->>>>>>> dcc418dbe7d0e116c8df53d4177c1786160860cc
-  threadItem: {
-    padding: 15,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-<<<<<<< HEAD
-=======
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    marginBottom: 10,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 5,
->>>>>>> dcc418dbe7d0e116c8df53d4177c1786160860cc
-  },
-  threadTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  threadInfo: {
-<<<<<<< HEAD
-    fontSize: 12,
-    color: '#555',
-  },
-  threadImage: {
-    width: 100,
-    height: 100,
-    marginTop: 10,
-    borderRadius: 5,
-  },
-  modalView: {
-    flex: 1,
-    backgroundColor: '#fff',
-    padding: 20,
-    marginTop: 50,
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-  },
-  closeButtonTopRight: {
-    position: 'absolute',
-    top: 10,
-    right: 10,
-  },
-  closeButtonText: {
-    fontSize: 18,
-    color: '#007BFF',
-=======
-    fontSize: 14,
-    color: 'gray',
-  },
-  modalView: {
-    flex: 1,
-    backgroundColor: 'white',
-    padding: 20,
-    margin: 20,
-    borderRadius: 10,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
->>>>>>> dcc418dbe7d0e116c8df53d4177c1786160860cc
-  },
-  modalTitle: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  modalContent: {
-    fontSize: 16,
-    marginBottom: 10,
-  },
-  modalInfo: {
-<<<<<<< HEAD
-    fontSize: 12,
-    color: '#555',
-    marginBottom: 10,
-  },
-  modalImage: {
-    width: 200,
-    height: 200,
-    marginBottom: 10,
-    borderRadius: 5,
-=======
-    fontSize: 14,
-    color: 'gray',
-    marginBottom: 20,
->>>>>>> dcc418dbe7d0e116c8df53d4177c1786160860cc
-  },
-  replyItem: {
-    padding: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-  },
-  replyInfo: {
-    fontSize: 12,
-<<<<<<< HEAD
-    color: '#555',
-  },
-  closeButton: {
-    marginTop: 10,
-=======
-    color: 'gray',
-  },
-  button: {
-    backgroundColor: '#007BFF',
-    padding: 10,
-    borderRadius: 5,
-    alignItems: 'center',
-    marginVertical: 5,
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 16,
-  },
-  closeButton: {
-    backgroundColor: '#6c757d',
-  },
-  closeButtonTopRight: {
-    position: 'absolute',
-    top: 10,
-    right: 10,
-    zIndex: 1,
-  },
-  closeButtonText: {
-    fontSize: 18,
-    color: 'red',
->>>>>>> dcc418dbe7d0e116c8df53d4177c1786160860cc
-  },
-});
+            <TextInput
+                style={styles.input}
+                placeholder="Password"
+                placeholderTextColor={isDarkTheme ? "white" : "black"}
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry
+            />
 
-export default Forum;
+            <TouchableOpacity
+                style={styles.buttonPrimary}
+                onPress={isRegistering ? handleRegister : handleLogin}
+            >
+                <Text style={styles.buttonText}>
+                    {isRegistering ? "Sign Up" : "Sign In"}
+                </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                style={styles.buttonSecondary}
+                onPress={() => setIsRegistering(!isRegistering)}
+            >
+                <Text style={styles.buttonTextSecondary}>
+                    {isRegistering ? "Sign In" : "Sign Up"}
+                </Text>
+            </TouchableOpacity>
+        </LinearGradient>
+    );
+}
