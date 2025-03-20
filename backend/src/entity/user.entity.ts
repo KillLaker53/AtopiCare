de02847feced6ec9e2e88a482c979f74b9a1266b
AvatarZorak <alexhristov2005@gmail.com>
2025-03-20 18:13:38 +0200

    Connection to PostgreSQL

---
+++ backend/src/entity/user.entity.ts
@@ -0,0 +1,29 @@
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Analysis } from './analysis.entity';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column({ type: 'text' })
+  first_name: string;
+
+  @Column({ type: 'text' })
+  last_name: string;
+
+  @Column({ type: 'text' })
+  username: string;
+
+  @Column({ type: 'text' })
+  email: string;
+
+  @Column({ type: 'text' })
+  password: string;
+
+  @Column({ type: 'enum', enum: ['reg', 'admin'] })
+  role: Role;
+
+  @OneToMany(type => Analysis, analysis => analysis.user)
+  analyses: Analysis[];
+}
--- backend/src/postgresql_connection/postgresql_connection.module.ts
+++ backend/src/postgresql_connection/postgresql_connection.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: `${process.env.postgres_host}`,
+      port: 5432,
+      username: `${process.env.postgres_user}`,
+      password: `${process.env.postgres_password}`,
+      database: `AtopiCare`,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+})
 export class PostgresqlConnectionModule {}
