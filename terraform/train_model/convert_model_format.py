a7dd84e48b000ff82ff43e071854439eed45c989
PepiDianabad <petar.y.petrov@elsys-bg.org>
2025-03-20 22:37:18 +0200

    working tar.gz format for the sagemaker

--- terraform/train_model/convert_model_format.py
+++ terraform/train_model/convert_model_format.py
@@ -1,11 +1,16 @@
 import os
 import tarfile
+import tensorflow as tf
 
-sagemaker_model_dir = "best_resnet_model"
+model = tf.keras.models.load_model("best_resnet_model.h5")
+
+sagemaker_model_dir = "sagemaker_model"
+os.makedirs(sagemaker_model_dir, exist_ok=True)
 
 model.save(sagemaker_model_dir)
 
-with tarfile.open("resnet_sagemaker_model.tar.gz", "w:gz") as tar:
+tar_path = "resnet_sagemaker_model.tar.gz"
+with tarfile.open(tar_path, "w:gz") as tar:
     tar.add(sagemaker_model_dir, arcname=os.path.basename(sagemaker_model_dir))
 
-print("Model saved and archived for SageMaker deployment!")
+print(f"✅ Model saved and archived as {tar_path} for SageMaker deployment!")
