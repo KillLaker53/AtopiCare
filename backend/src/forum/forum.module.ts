1a7fa06f024aa57a4a3cfbac61e075a4fcd4e9a3
AvatarZorak <alexhristov2005@gmail.com>
2025-03-21 00:16:44 +0200

    Resolved conflict

--- backend/src/forum/forum.module.ts
+++ backend/src/forum/forum.module.ts
@@ -5,11 +5,8 @@ import { MongodbConnectionModule } from '../mongodb_connection/mongodb_connectio
 import { ThreadSchema } from '../schema/thread.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostgresqlConnectionModule } from '../postgresql_connection/postgresql_connection.module';
-<<<<<<< HEAD
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../entity/user.entity';
-=======
->>>>>>> dcc418dbe7d0e116c8df53d4177c1786160860cc
 
 @Module({
   imports: [
@@ -22,10 +19,7 @@ import { User } from '../entity/user.entity';
       },
     ]),
     PostgresqlConnectionModule,
-<<<<<<< HEAD
     TypeOrmModule.forFeature([User]),
-=======
->>>>>>> dcc418dbe7d0e116c8df53d4177c1786160860cc
   ],
   controllers: [ForumController],
   providers: [ForumService],
