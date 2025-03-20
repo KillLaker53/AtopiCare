b8324529f1ff544585a4a26b5c730c612c1d272d
Stily <stilianqo@gmail.com>
2025-03-20 16:58:04 +0200

    feat: add placeholder upload page

--- AtopiCare/app/(tabs)/_layout.tsx
+++ AtopiCare/app/(tabs)/_layout.tsx
@@ -47,6 +47,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="UploadPhoto"
+        options={{
+          title: 'Upload Photo',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
