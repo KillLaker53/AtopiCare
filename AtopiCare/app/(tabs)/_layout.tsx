3bc512245586dc6062063f462b89489f57999ffb
aleks <aleksa_rashova@abv.bg>
2025-03-20 20:57:00 +0200

    feat: added option for changing the theme in the login and register screen

--- AtopiCare/app/(tabs)/_layout.tsx
+++ AtopiCare/app/(tabs)/_layout.tsx
@@ -3,59 +3,59 @@ import { View } from 'react-native';
 import AuthScreen from './AuthScreen';
 
 export default function TabLayout() {
-<<<<<<< HEAD
+// <<<<<<< HEAD
     return (
         <View style={{ flex: 1 }}>
             <AuthScreen />
         </View>
     );
-=======
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: 'Profile',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="UploadPhoto"
-        options={{
-          title: 'Upload Photo',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
->>>>>>> 37085ad6ef92ba12a408651378daf95062e7bb6a
+// =======
+//   const colorScheme = useColorScheme();
+//
+//   return (
+//     <Tabs
+//       screenOptions={{
+//         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+//         headerShown: false,
+//         tabBarButton: HapticTab,
+//         tabBarBackground: TabBarBackground,
+//         tabBarStyle: Platform.select({
+//           ios: {
+//             // Use a transparent background on iOS to show the blur effect
+//             position: 'absolute',
+//           },
+//           default: {},
+//         }),
+//       }}>
+//       <Tabs.Screen
+//         name="index"
+//         options={{
+//           title: 'Home',
+//           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+//         }}
+//       />
+//       <Tabs.Screen
+//         name="explore"
+//         options={{
+//           title: 'Explore',
+//           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+//         }}
+//       />
+//       <Tabs.Screen
+//         name="profile"
+//         options={{
+//           title: 'Profile',
+//           tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
+//         }}
+//       />
+//       <Tabs.Screen
+//         name="UploadPhoto"
+//         options={{
+//           title: 'Upload Photo',
+//           tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
+//         }}
+//       />
+//     </Tabs>
+//   );
+// >>>>>>> 37085ad6ef92ba12a408651378daf95062e7bb6a
 }
