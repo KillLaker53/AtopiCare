dffe9b55f539c63f04ad721b18cb4b0d83ddbc51
aleks <aleksa_rashova@abv.bg>
2025-03-21 02:52:34 +0200

    feat: created the screens for - profile and admin

--- AtopiCare/app/(tabs)/_layout.tsx
+++ AtopiCare/app/(tabs)/_layout.tsx
@@ -3,13 +3,17 @@ import { Platform, View, useColorScheme } from 'react-native';
 import AuthScreen from './AuthScreen';
 import UploadPhotoScreen from "@/app/(tabs)/UploadPhotoScreen";
 import AnalyzeScreen from "@/app/(tabs)/AnalyzeScreen";
+import AdminScreen from "@/app/(tabs)/AdminScreen";
+import ProfileScreen from "@/app/(tabs)/profile";
 
 export default function TabLayout() {
-    // return (
-    //     <View style={{flex: 1}}>
-    //         {/*<AuthScreen/>*/}
-    //         {/*<UploadPhotoScreen />*/}
-    //         <AnalyzeScreen />
-    //     </View>
-    // );
+    return (
+        <View style={{flex: 1}}>
+            {/*<AuthScreen/>*/}
+            {/*<UploadPhotoScreen />*/}
+            {/*<AnalyzeScreen />*/}
+            <AdminScreen />
+            {/*<ProfileScreen />*/}
+        </View>
+    );
 }
